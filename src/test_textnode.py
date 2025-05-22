5162ab60f14e71f3afbacd0a3df6716bbd44134d
RynerDyne <dominic@mightyexplorers.com>
2025-05-22 16:35:26 -0500

    complete chapter 2 lesson 2

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,15 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_texttype(self):
+        node = TextNode("This is a test case for code vs italics", TextType.CODE)
+        node2 = TextNode("This is a test case for code vs italics", TextType.ITALICS)
+        self.assertNotEqual(node, node2)
+
+    def test_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "http://boot.dev")
+        node2 = TextNode("This is a text node", TextType.BOLD, "http://boot.dev")
+        self.assertEqual(node, node2)
 
 if __name__ == "__main__":
     unittest.main()
