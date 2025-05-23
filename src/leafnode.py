2a96ab2217a7286e93c5578c2c397e43a55cf181
RynerDyne <dominic@mightyexplorers.com>
2025-05-23 15:08:15 -0500

    fix test issues

--- src/leafnode.py
+++ src/leafnode.py
@@ -2,7 +2,8 @@ from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, props)
+        super().__init__(tag, props)
+        self.value = value
 
     def to_html(self):
         if self.value is None:
--- src/parentnode.py
+++ src/parentnode.py
@@ -2,7 +2,8 @@ from htmlnode import HTMLNode
 
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
-        super().__init__(tag, children, props)
+        super().__init__(tag, props)
+        self.children = children
 
     def to_html(self):
         if self.tag is None:
@@ -10,10 +11,10 @@ class ParentNode(HTMLNode):
         elif self.children is None:
             raise ValueError("Enter a children argument")
         else:
-            html_txt = '<{tag}>'
+            html_txt = ''
             for child in self.children:
-                html_txt = html_txt + child.to_html()
-            html_txt = html_txt + '</{tag}>'
+                html_txt += child.to_html()
+            return f"<{self.tag}>{html_txt}</{self.tag}>"
 
     def __repr__(self):
         return f"ParentNode({self.tag}, {self.children}, {self.props})"
