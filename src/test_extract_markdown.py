0bca5210c80e4728cfb748f5441188bc8e0f567f
RynerDyne <dominic@mightyexplorers.com>
2025-05-29 09:33:52 -0500

    complete chapter 3 lesson 4

---
+++ src/test_extract_markdown.py
@@ -0,0 +1,29 @@
+import unittest
+
+from extract_markdown_images import extract_markdown_images
+from extract_markdown_links import extract_markdown_links
+
+class TestExtractMarkdown(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with an [link](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("link", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images_on_links(self):
+        matches = extract_markdown_images(
+            "This is text with an [image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([], matches)
+
+    def test_extract_markdown_links_on_images(self):
+        matches = extract_markdown_links(
+            "This is text with an ![images](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([], matches)
