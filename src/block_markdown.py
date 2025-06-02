7b2039178f44f969d466a0505ed053f6af24b638
RynerDyne <dominic@mightyexplorers.com>
2025-06-02 16:48:12 -0500

    complete chapter 4 lesson 2

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,7 +1,15 @@
-# Create a new function called markdown_to_blocks(markdown). It takes a raw Markdown string (representing a full document) as input and returns a list of "block" strings. 
-## The .split() method can be used to split a string into blocks based on a delimiter (\n\n is a double newline).
-## You should .strip() any leading or trailing whitespace from each block.
-## Remove any "empty" blocks due to excessive newlines.
+import re
+
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = 'paragraph'
+    HEADING = 'heading'
+    CODE = 'code'
+    QUOTE = 'quote'
+    UNORDERED_LIST = 'unordered_list'
+    ORDERED_LIST = 'ordered_list'
+
 def markdown_to_blocks(markdown):
     markdown_inline_list = list(map(
         lambda stripped: stripped.strip(),
@@ -10,3 +18,27 @@ def markdown_to_blocks(markdown):
         markdown.split("\n\n")
     )))
     return markdown_inline_list
+
+def block_to_block_type(markdown_block):
+    if re.fullmatch(r'#{1,6}.+', markdown_block):
+        return BlockType.HEADING
+    elif re.fullmatch(r'^`{3}.+`{3}$', markdown_block, re.MULTILINE.DOTALL):
+        return BlockType.CODE
+    elif re.fullmatch(r'^>.+', markdown_block, re.MULTILINE.DOTALL):
+        return BlockType.QUOTE
+    elif re.fullmatch(r'^- .+', markdown_block, re.MULTILINE.DOTALL):
+        return BlockType.UNORDERED_LIST
+    elif re.fullmatch(r'^\d+\. .+', markdown_block, re.MULTILINE.S):
+        split_markdown_block = markdown_block.split('\n')
+        count = 1
+        for split_line in split_markdown_block:
+            first = split_line[0]
+            if first == str(count):
+                if count == len(split_markdown_block):
+                    return BlockType.ORDERED_LIST
+                count += 1
+            else:
+                return BlockType.PARAGRAPH
+
+    else:
+        return BlockType.PARAGRAPH
---
+++ src/test_block_to_block_type.py
@@ -0,0 +1,92 @@
+import unittest
+import re
+
+from block_markdown import BlockType, block_to_block_type, markdown_to_blocks
+
+md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+
+#### Here is a header
+
+> quoted
+> text
+
+1. This
+2. is
+3. an
+4. ordered
+5. list
+
+```
+Here is a block of
+code
+```
+
+1. this
+2. list
+4. is
+3. out]
+5. of
+6. order
+"""
+blocks = markdown_to_blocks(md)
+
+# 0 p
+# 1 mlp
+# 2 unordered list
+
+# 3 Header
+# 4 quote
+# 5 ordered list
+# 6 code
+# 7 out of order ordered list
+
+class TestBlockTypePositive(unittest.TestCase):
+    def test_paragraph_block(self):
+        block_type = block_to_block_type(blocks[0])
+        self.assertEqual(block_type,
+                      BlockType.PARAGRAPH)
+
+    def test_multiline_paragraph_block(self):
+        block_type = block_to_block_type(blocks[1])
+        self.assertEqual(block_type,
+                      BlockType.PARAGRAPH)
+
+    def test_heading_block(self):
+        block_type = block_to_block_type(blocks[3])
+        self.assertEqual(block_type,
+                      BlockType.HEADING)
+
+    def test_code_block(self):
+        block_type = block_to_block_type(blocks[6])
+        self.assertEqual(block_type,
+                      BlockType.CODE)
+
+    def test_quote_block(self):
+        block_type = block_to_block_type(blocks[4])
+        self.assertEqual(block_type,
+                      BlockType.QUOTE)
+ 
+    def test_unordered_list(self):
+        block_type = block_to_block_type(blocks[2])
+        self.assertEqual(block_type,
+                      BlockType.UNORDERED_LIST)
+
+    def test_ordered_list(self):
+        block_type = block_to_block_type(blocks[5])
+        self.assertEqual(block_type,
+                         BlockType.ORDERED_LIST)
+
+    def test_ordered_list_out_of_order(self):
+        block_type = block_to_block_type(blocks[7])
+        self.assertEqual(block_type,
+                         BlockType.PARAGRAPH)
+
+if __name__ == '__main__':
+    unittest.main()
